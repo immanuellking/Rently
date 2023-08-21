fd433455d66ee6e12b2f5ef7ba9f907b47537e42
immanuellking <emmaebubeegbosi@gmail.com>
2023-08-21 16:02:00 +0100

    done with faq section, not yet responsive

--- src/components/CommonQuestions.jsx
+++ src/components/CommonQuestions.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BsChevronDown } from "react-icons/bs";
+
+const faqsData = [
+  {
+    tab: "How does Spleet Work?",
+    id: 1,
+    isOpen: false,
+    text: (
+      <div className="font-medium text-[#6d6d6e] space-y-6">
+        <p>
+          At Spleet, we offer access to premium residential solutions with
+          options of monthly, quarterly and biannual subscription. Once you find
+          a space you like, simply create a booking along with a few details
+          about yourself. This request is then processed within a few hours.
+        </p>
+        <p>
+          No payment is taken until the booking is accepted. We will charge you
+          the rent upfront plus a one-off booking fee and security deposit. You
+          will then receive a confirmation email with the details of your new
+          space.
+        </p>
+      </div>
+    ),
+  },
+  {
+    tab: "Does Spleet organise viewings?",
+    id: 2,
+    isOpen: false,
+    text: (
+      <div className="font-medium text-[#6d6d6e]">
+        <p>Yes.</p>
+      </div>
+    ),
+  },
+  {
+    tab: "Does Spleet own the spaces listed?",
+    id: 3,
+    isOpen: false,
+    text: (
+      <div className="font-medium text-[#6d6d6e]">
+        <p>
+          No Spleet does not own the spaces listed, as we have homeowners that
+          list these spaces on our platform.
+        </p>
+      </div>
+    ),
+  },
+];
 
 export const CommonQuestions = () => {
+  const [faqs, setFaqs] = useState(faqsData);
+  const displayFaq = (id) => {
+    const updatedFaq = faqs.map((faq) => {
+      return faq.id === id ? { ...faq, isOpen: !faq.isOpen } : faq;
+    });
+
+    setFaqs(updatedFaq);
+  };
+
   return (
     <section className="bg-[#F9FAFD] px-2 sm:px-5 lg:px-20 py-14 sm:py-20 space-y-20">
+      <div>
+        <div className="space-y-4">
+          <h3 className="text-[#059A57] font-bold">COMMON QUESTIONS</h3>
+          <div className="space-y-4 sm:space-y-6 lg:space-y-8">
+            <h1 className="text-black text-2xl sm:text-4xl font-bold">
+              Frequently asked questions
+            </h1>
+            <p className="w-[75%] text-sm text-[#6d6d6e] font-semibold">
+              Get quick answers to all your questions and concerns about Spleet
+              and Spleet homes. Whether as a member, host or just a visitor, we
+              will have an answer waiting for you.
+            </p>
+            <button className="bg-brightBlue py-4 px-10 text-white text-sm font-semibold rounded-full">
+              Visit FAQS
+            </button>
+          </div>
+        </div>
+      </div>
 
+      <div className="space-y-8">
+        {faqs.map((faq, index) => {
+          const { tab, text, id, isOpen } = faq;
+          return (
+            <div
+              key={index}
+              className="space-y-6 border-b-2 border-[#E6E9FB] pb-5"
+            >
+              <div className="flex justify-between">
+                <h1 className="text-lg font-bold m-0">{tab}</h1>
+                <div
+                  className="flex justify-center items-center w-10 h-10 bg-[#E6E9FB] rounded-full"
+                  onClick={() => displayFaq(id)}
+                >
+                  <BsChevronDown
+                    size={18}
+                    className={`font-extrabold text-black transition-all duration-300 ${isOpen && "rotate-180"}`}
+                  />
+                </div>
+              </div>
+
+              <div className={` ${isOpen ? "flex duration-500 transition-all" : "hidden"}`}>{text}</div>
+            </div>
+          );
+        })}
+      </div>
     </section>
-  )
-}
+  );
+};
