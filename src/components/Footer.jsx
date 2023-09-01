1e9d98aac3bdbb79d34b8c9397b96387c8548724
immanuellking <emmaebubeegbosi@gmail.com>
2023-09-01 04:36:31 +0100

    done with the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,9 +29,9 @@ const footerContents = [
 
 const Footer = () => {
   return (
-    <footer className="text-white px-20 pt-20 pb-10">
-      <div className="flex justify-between space-x-20 pb-10 border-b-[0.5px] border-b-[#3e3e3f]">
-        <div className="flex-[30%] flex flex-col justify-between">
+    <footer className="text-white px-5 lg:px-20 pt-20 pb-10 w-full">
+      <div className="flex flex-col lg:justify-between space-y-10 lg:space-y-0 lg:space-x-20 pb-10 border-b-[0.5px] border-b-[#3e3e3f]">
+        <div className="flex-1 sm:w-[50%] lg:w-full lg:flex-[30%] flex flex-col justify-between space-y-3 lg:space-y-0">
           <h1 className="text-4xl font-semibold header cursor-default">
             Rently.
           </h1>
@@ -43,19 +43,19 @@ const Footer = () => {
             your property and apply for rental loans. How can we help you today?
           </p>
         </div>
-        <div className="flex justify-between flex-[60%]">
+        <div className="flex flex-wrap sm:flex-nowrap justify-between lg:flex-[60%] gap-y-14 sm:gap-y-0">
           {footerContents.map((footerContent, index) => {
             const { heading, links } = footerContent;
             return (
               <div
                 key={index}
-                className="flex-1 space-y-6"
+                className="lg:flex-1 space-y-6"
               >
-                <h3 className="text-sm font-semibold">{heading}</h3>
+                <h3 className="text-base font-bold">{heading}</h3>
                 {links.map((link, lindex) => (
                   <p
                     key={link}
-                    className={`text-sm font-semibold w-fit cursor-pointer ${
+                    className={`text-sm font-semibold w-64 lg:w-fit cursor-pointer ${
                       heading === "GET IN TOUCH" && lindex === 2
                         ? "hover:text-white"
                         : "hover:text-brightGreen"
@@ -69,8 +69,8 @@ const Footer = () => {
           })}
         </div>
       </div>
-      <div className="flex justify-between mt-10 px-12">
-        <div className="flex gap-x-20">
+      <div className="flex flex-col lg:justify-between gap-y-8 lg:gap-y-0 mt-7 sm:mt-10 sm:px-5 lg:px-12">
+        <div className="flex flex-col sm:flex-row gap-y-5 sm:gap-x-10 lg:gap-x-20">
           <p className="text-sm font-semibold cursor-pointe">
             &copy;Rently Inc, 2023
           </p>
