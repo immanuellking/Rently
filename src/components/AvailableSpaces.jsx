f9135a95349e954ef3fb1f7c5bef19056b7ff419
immanuellking <emmaebubeegbosi@gmail.com>
2024-01-15 19:25:41 +0100

    worked on pagination buttons, their hover state and active state and functionality

--- src/components/AvailableSpaces.jsx
+++ src/components/AvailableSpaces.jsx
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchApartments } from "../features/Rent/apartmentsSlice";
 import Space from "./Space";
 
+import { FaGreaterThan } from "react-icons/fa6";
+import { FaLessThan } from "react-icons/fa6";
+
 const AvailableSpaces = () => {
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -14,7 +17,7 @@ const AvailableSpaces = () => {
 
   const totalSpace = available_apartments.length;
   const spacePerPage = 6;
-  const pages = Math.floor(totalSpace / spacePerPage);
+  const pages = Math.ceil(totalSpace / spacePerPage);
 
   const goToPrev = () => {
     const prevPage = Math.max(currentPage - 1, 1);
@@ -30,15 +33,37 @@ const AvailableSpaces = () => {
   const end = spacePerPage * currentPage;
   const apartments = available_apartments.slice(start, end);
 
+  const pageButtons = Array.from({ length: pages });
+  console.log(pageButtons);
+
   useEffect(() => {
     dispatch(fetchApartments());
   }, []);
 
   return (
-    <div className="w-full grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5 bg-[#F4F4F4] px-5 lg:px-14 py-14 lg:py-16">
-      {apartments.map((apartment, index) => {
-        return <Space key={index} {...apartment} />;
-      })}
+    <div className="w-full bg-[#F4F4F4] px-5 lg:px-14 py-14 lg:py-16">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5 pb-14 lg:pb-16">
+        {apartments.map((apartment, index) => {
+          return <Space key={index} {...apartment} />;
+        })}
+      </div>
+      <div className=" w-full flex items-center justify-center gap-x-2">
+        <div className="w-10 h-10 border-[1.5px] border-gray-300 rounded-full flex items-center justify-center cursor-pointer text-gray-500 font-semibold hover:bg-brightBlue hover:border-brightBlue hover:text-white duration-300 ease-in-out" onClick={goToPrev}>
+          <FaLessThan />
+        </div>
+        <div className="flex items-center gap-x-2">
+          {pageButtons.map((_, index) => {
+            return (
+              <div className={`${currentPage === index + 1 ? "bg-brightBlue border-brightBlue text-white" : "border-gray-300 bg-transparent text-gray-500"} w-10 h-10 border-[1.5px] rounded-full flex items-center justify-center cursor-pointer font-semibold hover:bg-brightBlue hover:border-brightBlue hover:text-white duration-300 ease-in-out`}>
+                {index + 1}
+              </div>
+            );
+          })}
+        </div>
+        <div className="w-10 h-10 border-[1.5px] border-gray-300 rounded-full flex items-center justify-center cursor-pointer text-gray-500 font-semibold hover:bg-brightBlue hover:border-brightBlue hover:text-white duration-300 ease-in-out" onClick={goToNext}>
+          <FaGreaterThan />
+        </div>
+      </div>
     </div>
   );
 };
